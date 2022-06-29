f5657f43650bae667b43947dd1e65830aac3cb64
RÔMULO FRANKLIN DE MEIRA RAMOS <romulo.ramos@gee.inatel.br>
2022-06-29 16:24:36 +0100

    Aula 05 - Integração do Codegen para gerar schemas e hoocks a partir da API Graphql-GraphCMS.

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,35 +1,8 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css'
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-    query GetLessonBySlug ($slug: String) {
-        lesson(where: {slug: $slug}) {
-            title
-            id
-            description
-            teacher {
-            name
-            bio
-            avatarURL
-        }
-    }
-}`
-
-interface GetLessonBySlugResponse {
-    lesson: {
-        title: string;
-        id: string;
-        description: string;
-        teacher: {
-            name: string;
-            bio: string;
-            avatarURL: string;
-        }
-    }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
     lessonSlug: string;
@@ -37,7 +10,7 @@ interface VideoProps {
 
 export function Video(props: VideoProps){
 
-    const {data} =  useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    const {data} =  useGetLessonBySlugQuery( {
         variables: {
             slug: props.lessonSlug,
         }
@@ -45,7 +18,7 @@ export function Video(props: VideoProps){
 
     console.log(data);
 
-    if(!data){
+    if(!data || !data.lesson){
         return(
             <div className="flex-1">
                 <p>Carregando...</p>
@@ -72,19 +45,21 @@ export function Video(props: VideoProps){
                         <p className="mt-4 text-gray-200 leading-relaxed">
                             {data.lesson.description}
                         </p>
-                        <div className="flex items-center gap-4 mt-4">
-                            
-                            <img 
-                                className="h-16 w-16 rounded-full border-2 border-blue-500" 
-                                src={data.lesson.teacher.avatarURL} 
-                                alt="" 
-                            />
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                
+                                <img 
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500" 
+                                    src={data.lesson.teacher.avatarURL} 
+                                    alt="" 
+                                />
 
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                     <div className="flex flex-col gap-4">
                         <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
